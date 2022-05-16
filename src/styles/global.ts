b1eabf4aa5b84ad6c0e5e4a73907b9743978fe1c
Mário Santos <mariodev7@gmail.com>
2022-05-16 20:08:25 -0300

    setup fonts from google fonts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
         --red: #e52b40;
         --blue: #5429CC;
         --blue-light: #6933ff;
-        
+
         --text-title: #363f5f;
         --text-body: #969cb3;
 
@@ -32,6 +32,15 @@ export const GlobalStyle = createGlobalStyle`
     body {
         background: var(--background);
         -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 400;
     }
 
     button {
