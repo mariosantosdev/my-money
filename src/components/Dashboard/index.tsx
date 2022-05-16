1bfd1d92029b353ff87a3d06282e6abecbd45de7
Mário Santos <mariodev7@gmail.com>
2022-05-16 20:43:56 -0300

    create dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { Summarry } from "../Summary";
+import { Container } from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Summarry />
+    </Container>
+  );
+}
