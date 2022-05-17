244eab535d82d732a25716d767b18f846888bda4
Mário Santos <mariodev7@gmail.com>
2022-05-16 22:14:37 -0300

    create modal new transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { Fragment, useRef } from "react";
 
 import LogoImg from "../../assets/logo.svg";
+import ModalNewTransaction, {
+  ModalTransactionHandles,
+} from "../ModalNewTransaction";
 import { Container, Content } from "./styles";
 
 export function Header() {
-  return (
-    <Container>
-      <Content>
-        <img src={LogoImg} alt="my money logo" />
+  const modalTransactionRef = useRef<ModalTransactionHandles>(null);
 
-        <button type="button">Nova Transação</button>
-      </Content>
-    </Container>
+  function handleOpenTransactionModal() {
+    if (!modalTransactionRef.current) return null;
+
+    modalTransactionRef.current.openModal();
+  }
+
+  return (
+    <Fragment>
+      <Container>
+        <Content>
+          <img src={LogoImg} alt="my money logo" />
+
+          <button type="button" onClick={handleOpenTransactionModal}>
+            Nova Transação
+          </button>
+        </Content>
+      </Container>
+      <ModalNewTransaction ref={modalTransactionRef} />
+    </Fragment>
   );
 }
---
+++ src/components/ModalNewTransaction/index.tsx
@@ -0,0 +1,111 @@
+import React, {
+  FormEvent,
+  forwardRef,
+  ForwardRefRenderFunction,
+  useCallback,
+  useImperativeHandle,
+  useState,
+} from "react";
+import Modal from "react-modal";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
+
+import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+
+export interface ModalTransactionHandles {
+  openModal: () => void;
+}
+
+Modal.setAppElement("#root");
+
+const ModalNewTransaction: ForwardRefRenderFunction<ModalTransactionHandles> = (
+  _,
+  ref
+) => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("deposit");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
+  const handleClose = useCallback(() => {
+    setIsVisible(false);
+  }, []);
+
+  const openModal = useCallback(() => {
+    setIsVisible(true);
+  }, []);
+
+  useImperativeHandle(ref, () => ({ openModal }));
+
+  const handleCreateTransaction = useCallback((event: FormEvent) => {
+    event.preventDefault();
+  }, []);
+
+  if (!isVisible) return null;
+
+  return (
+    <Modal
+      isOpen={isVisible}
+      onRequestClose={handleClose}
+      contentLabel="Modal Nova Transação"
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <button type="button" className="react-modal-close" onClick={handleClose}>
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+      <Container onSubmit={handleCreateTransaction}>
+        <h2>Cadastrar transações</h2>
+
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={(e) => setTitle(e.target.validationMessage)}
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={(e) => setValue(Number(e.target.value))}
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => setType("deposit")}
+            isActive={type === "deposit"}
+            activeColor="#33cc95"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </RadioBox>
+          <RadioBox
+            type="button"
+            onClick={() => setType("withdraw")}
+            isActive={type === "withdraw"}
+            activeColor="#e52b40"
+          >
+            <img src={outcomeImg} alt="Saida" />
+            <span>Saida</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
+    </Modal>
+  );
+};
+
+export default forwardRef(ModalNewTransaction);
