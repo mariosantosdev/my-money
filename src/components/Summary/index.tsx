bb131244fb52169e8a41610c1c4aef421f67b849
Mário Santos <mariodev7@gmail.com>
2022-05-16 23:08:10 -0300

    using transactions values in summary cards

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 
 import { Container } from "./styles";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
+import { TransactionContext } from "../../contexts/TransactionContext";
 
 export function Summarry() {
+  const { transactions } = useContext(TransactionContext);
+
+  const { deposits, total, withdraws } = useMemo(() => {
+    return transactions.reduce(
+      (accumulator, transaction) => {
+        if (transaction.type === "deposit") {
+          accumulator.deposits += transaction.value;
+          accumulator.total += transaction.value;
+        } else {
+          accumulator.withdraws += transaction.value;
+          accumulator.total -= transaction.value;
+        }
+
+        return accumulator;
+      },
+      { deposits: 0, withdraws: 0, total: 0 }
+    );
+  }, [transactions]);
+
   return (
     <Container>
       <div>
@@ -14,7 +34,12 @@ export function Summarry() {
           <img src={incomeImg} alt="Entradas" />
         </header>
 
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            currency: "BRL",
+            style: "currency",
+          }).format(deposits)}
+        </strong>
       </div>
       <div>
         <header>
@@ -22,7 +47,12 @@ export function Summarry() {
           <img src={outcomeImg} alt="Saidas" />
         </header>
 
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            currency: "BRL",
+            style: "currency",
+          }).format(withdraws)}
+        </strong>
       </div>
       <div className="hightlight-background">
         <header>
@@ -30,7 +60,12 @@ export function Summarry() {
           <img src={totalImg} alt="Total" />
         </header>
 
-        <strong>R$1000,00</strong>
+        <strong>
+          {new Intl.NumberFormat("pt-br", {
+            currency: "BRL",
+            style: "currency",
+          }).format(total)}
+        </strong>
       </div>
     </Container>
   );
